a5f9aaa5b335417641e48a75a3d96ac471c0b186
Adam Clements <adam.clements@gmail.com>
2012-05-07 02:11:10 +0100

    Basic drawing and some behaviours

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject Pong "0.0.1-SNAPSHOT"
+  :description "Attempt at a Pong game using clojure"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/clojure-contrib "1.1.0"]
+                 [quil "1.4.1"]]
+  :dev-dependencies [[lein-eclipse "1.0.0"]])
---
+++ test/Pong/test/core.clj
@@ -0,0 +1,6 @@
+(ns Pong.test.core
+  (:use [Pong.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
